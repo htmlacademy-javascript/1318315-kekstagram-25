f9751fa5465cde8a7486b03e6d45e55deea426d4
Zoy <zjgolub@gmail.com>
2022-02-23 11:41:14 +0200

    Исправила условие проверки чисел в функции получения случайного числа

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const getRandomNumber = function (min, max) {
-  if (min < 0 && min >= max) {
-    return 'Ошибка при введении чисел';
+  if (0 <= min < max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+  return 'Ошибка при введении чисел';
 };
 
 getRandomNumber(0, 15);
