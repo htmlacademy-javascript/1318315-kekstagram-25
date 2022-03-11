3949ec53ae5b039e53a3105207ecece0bbcadb96
ZoyaGolub <zjgolub@gmail.com>
2022-03-11 16:51:42 +0200

    Изменила код для получения количества комментариев

--- js/rendering-photos.js
+++ js/rendering-photos.js
@@ -5,14 +5,12 @@ const data = createArrayPhotos();
 const templateFragment = document.querySelector('#picture').content;
 const templatePicture = templateFragment.querySelector('.picture');
 const pictures = document.querySelector('.pictures');
-//console.log(pictures);
 
 // Создание одного фото
 const createPhoto = (photo) => {
-  //console.log(photo);
   const img = templatePicture.cloneNode(true);
   img.querySelector('.picture__img').src = photo.url;
-  img.querySelector('.picture__comments').textContent = photo.comments;
+  img.querySelector('.picture__comments').textContent = photo.comments.length;
   img.querySelector('.picture__likes').textContent = photo.likes;
   return img;
 };
@@ -20,15 +18,11 @@ const createPhoto = (photo) => {
 // Создание массива фотографий
 const drawArrayPhotos = (photos) => {
   const fragment = document.createDocumentFragment();
-  for(let i = 0; i <= photos.length; i++) {
+  for(let i = 0; i < photos.length; i++) {
     const newPhoto = createPhoto(photos[i]);
     fragment.appendChild(newPhoto);
   }
   pictures.appendChild(fragment);
 };
 
-//console.log(pictures);
-
 drawArrayPhotos(data);
-
-//console.log(drawArrayPhotos(data));
