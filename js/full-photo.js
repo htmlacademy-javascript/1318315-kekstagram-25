701030906a49510fc36c52396bb430ec21a95d32
ZoyaGolub <zjgolub@gmail.com>
2022-04-01 08:26:57 +0300

    Удалила дублирующиеся функции по созданию комментариев

--- js/full-photo.js
+++ js/full-photo.js
@@ -1,29 +1,8 @@
-import {createArrayComments} from './model-photos.js';
-
-const dataComments = createArrayComments();
-
 const fullScreenPhoto = document.querySelector('.big-picture');
 const body = document.querySelector('body');
 
 const IMG_WIDTH = 35;
 const IMG_HEIGHT = 35;
-
-// Обнуляю комментарии записанные ранее
-const deleteComments = () => {
-  const comments = fullScreenPhoto.querySelector('.social__comments');
-  const items = comments.children;
-  //console.log(items);
-  if (items.length > 0) {
-    for (let i = items.length; i > 0; i--) {
-      //console.log(number);
-      //console.log(items[0]);
-      items[0].remove();
-      //console.log(items);
-    }
-  }
-  //console.log(items);
-  return items;
-};
 
 // Подстановка одного комментария
 const insertComment = (object) => {
@@ -50,17 +29,14 @@ const insertComment = (object) => {
 // Функция по подстановке комментариев
 const insertComments = (comments) => {
   const commentsArray = fullScreenPhoto.querySelector('.social__comments');
+  commentsArray.textContent = '';
+
   for (let i = 0; i < comments.length; i++) {
     const newComment = insertComment(comments[i]);
     commentsArray.appendChild(newComment);
   }
   return commentsArray;
 };
-
-const commentsPhoto = insertComments(dataComments);
-// console.log(commentsPhoto.children);
-const commentsPhotoToObject = Array.from(commentsPhoto);
-// console.log(commentsPhotoToObject);
 
 // Функция-шаблон отрисовки полноэкранного фото
 const drawFullScreenPhoto = (photo) => {
@@ -72,22 +48,10 @@ const drawFullScreenPhoto = (photo) => {
   fullScreenPhoto.querySelector('.big-picture__img img').src = photo.url;
   fullScreenPhoto.querySelector('.likes-count').textContent = '';
   fullScreenPhoto.querySelector('.likes-count').append(photo.likes);
-
-  //console.log(fullScreenPhoto.querySelector('.comments-count').append(photo.comments.length));
-  fullScreenPhoto.querySelector('.comments-count').append(photo.comments.length);  //  Не подставляется количество комментариев
-  fullScreenPhoto.querySelector('.comments-count').append(`photo.${commentsPhoto}.length`);  //  Не подставляется количество комментариев
-  fullScreenPhoto.querySelector('.comments-count').append(`photo.${commentsPhotoToObject}.length`);  //  Не подставляется количество комментариев
-
   fullScreenPhoto.querySelector('.social__caption').append(photo.description);
-
-  deleteComments();
-  // console.log(fullScreenPhoto.querySelector('.social__comments').replaceWith(photo.comments));
-  // console.log(fullScreenPhoto.querySelector('.social__comments').replaceWith(commentsPhoto));
-  // console.log(fullScreenPhoto.querySelector('.social__comments').replaceWith(`photo.${commentsPhotoToObject}`));
-  fullScreenPhoto.querySelector('.social__comments').replaceWith(`photo.${commentsPhotoToObject}`); //  Не подставляется массив комментариев
-  fullScreenPhoto.querySelector('.social__comments').replaceWith(`photo.${commentsPhoto}`); //  Не подставляется массив комментариев
-  fullScreenPhoto.querySelector('.social__comments').replaceWith(commentsPhoto);  //  Не подставляется массив комментариев
-  fullScreenPhoto.querySelector('.social__comments').replaceWith(photo.comments); //  Не подставляется массив комментариев
+  fullScreenPhoto.querySelector('.comments-count').textContent = photo.comments.length;
+  const cureentComments = insertComments(photo.comments);
+  fullScreenPhoto.querySelector('.social__comments').replaceWith(cureentComments);
 };
 
 export {drawFullScreenPhoto, fullScreenPhoto, body};
