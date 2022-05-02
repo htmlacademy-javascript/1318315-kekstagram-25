0ab96cc41105fca27a40db206f811b2e0630aea4
Zoy <zjgolub@gmail.com>
2022-05-02 09:18:06 +0300

    Объединила обработчики - ошибка в консоли

--- js/effects-photo.js
+++ js/effects-photo.js
@@ -38,57 +38,6 @@ scaleSmaller.addEventListener('click', toEnlargePhoto);
 scaleMore.addEventListener('click', toReducePhoto);
 
 
-// Наложение эффекта на загружаемую фотографию
-
-const toAddEffects = (effect, value) => {
-  switch (effect) {
-    case 'none':
-      photoPreview.style.filter = '';
-      slider.setAttribute('disabled', '');
-      sliderControl.setAttribute('disabled', '');
-      levelEffect.setAttribute('disabled' ,'');
-      break;
-    case 'chrome':
-      slider.removeAttribute('disabled');
-      sliderControl.removeAttribute('disabled');
-      levelEffect.removeAttribute('disabled');
-      photoPreview.style.filter = `grayscale(${value})`;
-      break;
-    case 'sepia':
-      slider.removeAttribute('disabled');
-      sliderControl.removeAttribute('disabled');
-      levelEffect.removeAttribute('disabled');
-      photoPreview.style.filter = `sepia(${value})`;
-      break;
-    case 'marvin':
-      slider.removeAttribute('disabled');
-      sliderControl.removeAttribute('disabled');
-      levelEffect.removeAttribute('disabled');
-      photoPreview.style.filter = `invert(${value}%)`;
-      break;
-    case 'phobos':
-      slider.removeAttribute('disabled');
-      sliderControl.removeAttribute('disabled');
-      levelEffect.removeAttribute('disabled');
-      photoPreview.style.filter = `blur(${value}px)`;
-      break;
-    case 'heat':
-      slider.removeAttribute('disabled');
-      sliderControl.removeAttribute('disabled');
-      levelEffect.removeAttribute('disabled');
-      photoPreview.style.filter = `brightness(${value})`;
-      break;
-  }
-};
-
-const selectedEffect = (evt) => {
-  evt.preventDefault();
-  toAddEffects(evt.target.value, sliderControl.noUiSlider.get());
-};
-
-effects.addEventListener('change', selectedEffect);
-
-
 // Регулировка эффекта слайдером
 
 noUiSlider.create(sliderControl, {
@@ -178,6 +127,75 @@ const heatUpdateOptions = () => {
   });
 };
 
+
+// Наложение эффекта на загружаемую фотографию
+
+const toAddEffects = (effect, value) => {
+  switch (effect) {
+    case 'none':
+      slider.setAttribute('disabled', '');
+      sliderControl.setAttribute('disabled', '');
+      levelEffect.setAttribute('disabled' ,'');
+      noneUpdateOptions();
+      photoPreview.style.filter = '';
+      photoPreview.removeAttribute('class');
+      break;
+    case 'chrome':
+      slider.removeAttribute('disabled');
+      sliderControl.removeAttribute('disabled');
+      levelEffect.removeAttribute('disabled');
+      photoPreview.style.filter = `grayscale(${value})`;
+      chromeUpdateOptions();
+      photoPreview.removeAttribute('class');
+      photoPreview.classList.add('effects__preview--chrome');
+      break;
+    case 'sepia':
+      slider.removeAttribute('disabled');
+      sliderControl.removeAttribute('disabled');
+      levelEffect.removeAttribute('disabled');
+      photoPreview.style.filter = `sepia(${value})`;
+      sepiaUpdateOptions();
+      photoPreview.removeAttribute('class');
+      photoPreview.classList.add('effects__preview--sepia');
+      break;
+    case 'marvin':
+      slider.removeAttribute('disabled');
+      sliderControl.removeAttribute('disabled');
+      levelEffect.removeAttribute('disabled');
+      photoPreview.style.filter = `invert(${value}%)`;
+      marvinUpdateOptions();
+      photoPreview.removeAttribute('class');
+      photoPreview.classList.add('effects__preview--marvin');
+      break;
+    case 'phobos':
+      slider.removeAttribute('disabled');
+      sliderControl.removeAttribute('disabled');
+      levelEffect.removeAttribute('disabled');
+      photoPreview.style.filter = `blur(${value}px)`;
+      phobosUpdateOptions();
+      photoPreview.removeAttribute('class');
+      photoPreview.classList.add('effects__preview--phobos');
+      break;
+    case 'heat':
+      slider.removeAttribute('disabled');
+      sliderControl.removeAttribute('disabled');
+      levelEffect.removeAttribute('disabled');
+      photoPreview.style.filter = `brightness(${value})`;
+      heatUpdateOptions();
+      photoPreview.removeAttribute('class');
+      photoPreview.classList.add('effects__preview--heat');
+      break;
+  }
+};
+
+const selectedEffect = (evt) => {
+  evt.preventDefault();
+  toAddEffects(evt.target.value, sliderControl.noUiSlider.get());
+};
+
+effects.addEventListener('change', selectedEffect);
+
+
 sliderControl.noUiSlider.on('update', () => {
   const value = sliderControl.noUiSlider.get();
   levelEffect.value = value;
@@ -187,53 +205,5 @@ sliderControl.noUiSlider.on('update', () => {
   toAddEffects(effect, value);
 });
 
-
-effects.addEventListener('change', (evt) => {
-  const value = evt.target.checked;
-  if (value) {
-    switch (evt.target.value) {
-      case 'none':
-        noneUpdateOptions();
-        photoPreview.style.filter = '';
-        photoPreview.removeAttribute('class');
-        break;
-      case 'chrome':
-        chromeUpdateOptions();
-        slider.classList.remove('visually-hidden');
-        photoPreview.style.filter = `grayscale(${value})`;
-        photoPreview.removeAttribute('class');
-        photoPreview.classList.add('effects__preview--chrome');
-        break;
-      case 'sepia':
-        sepiaUpdateOptions();
-        slider.classList.remove('visually-hidden');
-        photoPreview.style.filter = `sepia(${value})`;
-        photoPreview.removeAttribute('class');
-        photoPreview.classList.add('effects__preview--sepia');
-        break;
-      case 'marvin':
-        marvinUpdateOptions();
-        slider.classList.remove('visually-hidden');
-        photoPreview.style.filter = `invert(${value}%)`;
-        photoPreview.removeAttribute('class');
-        photoPreview.classList.add('effects__preview--marvin');
-        break;
-      case 'phobos':
-        phobosUpdateOptions();
-        slider.classList.remove('visually-hidden');
-        photoPreview.style.filter = `blur(${value}px)`;
-        photoPreview.removeAttribute('class');
-        photoPreview.classList.add('effects__preview--phobos');
-        break;
-      case 'heat':
-        heatUpdateOptions();
-        slider.classList.remove('visually-hidden');
-        photoPreview.style.filter = `brightness(${value})`;
-        photoPreview.removeAttribute('class');
-        photoPreview.classList.add('effects__preview--heat');
-        break;
-    }
-  }
-});
 
 export {scaleSmaller, scaleMore, photoPreview, effects, slider, toEnlargePhoto, toReducePhoto, selectedEffect};
